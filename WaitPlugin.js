cee718f5399ba34b46d7fd09acb07aa0e7be25b5
alex baldwin <ghostbaldwin@gmail.com>
2020-06-21 22:23:05 -0400

    fix build

---
+++ WaitPlugin.js
@@ -0,0 +1,25 @@
+const WebpackBeforeBuildPlugin = require('before-build-webpack');
+const fs = require('fs');
+
+/**
+ * https://www.viget.com/articles/run-multiple-webpack-configs-sequentially/
+ */
+module.exports = class WaitPlugin extends WebpackBeforeBuildPlugin {
+  constructor(file, interval = 100, timeout = 10000) {
+    super((_stats, callback) => {
+      const start = Date.now();
+
+      function poll() {
+        if (fs.existsSync(file)) {
+          callback();
+        } else if (Date.now() - start > timeout) {
+          throw Error("Maybe it just wasn't meant to be.");
+        } else {
+          setTimeout(poll, interval);
+        }
+      }
+
+      poll();
+    });
+  }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,114 +1,113 @@
 const path = require('path');
 const StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
 const LoadablePlugin = require('@loadable/webpack-plugin');
+const WaitPlugin = require('./WaitPlugin');
 
-module.exports = () => {
-  const resolve = {
-    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-  };
+const resolve = {
+  modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+};
 
-  const plugins = [
-    new LoadablePlugin({ writeToDisk: true }),
-  ];
+const plugins = [];
 
-  const rules = [
-    {
-      test: /\.jsx?$/,
-      use: {
-        loader: 'babel-loader',
+const rules = [
+  {
+    test: /\.jsx?$/,
+    use: {
+      loader: 'babel-loader',
+    },
+  },
+  {
+    test: /\.svg$/,
+    use: {
+      loader: '@svgr/webpack',
+      options: {
+        titleProp: true,
       },
     },
-    {
-      test: /\.svg$/,
-      use: {
-        loader: '@svgr/webpack',
-        options: {
-          titleProp: true,
-        },
-      },
+  },
+  {
+    test: /\.(woff2?|ttf|otf|eot)$/,
+    use: {
+      loader: 'file-loader',
     },
-    {
-      test: /\.(woff2?|ttf|otf|eot)$/,
-      use: {
-        loader: 'file-loader',
-      },
+  },
+  {
+    test: /\.(png|jpe?g|gif|ico)$/,
+    use: {
+      loader: 'file-loader',
     },
-    {
-      test: /\.(png|jpe?g|gif|ico)$/,
-      use: {
-        loader: 'file-loader',
-      },
+  },
+  {
+    test: /\.md$/,
+    use: {
+      loader: 'raw-loader',
     },
-    {
-      test: /\.md$/,
-      use: {
-        loader: 'raw-loader',
-      },
-    },
-  ];
+  },
+];
 
-  return [
-    {
-      entry: {
-        client: path.resolve(__dirname, 'src/client.js'),
-      },
-      output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        chunkFilename: '[name].js',
-        publicPath: '/',
-      },
-      devServer: {
-        port: 3000,
-        contentBase: './dist',
-        historyApiFallback: true,
-      },
-      resolve: resolve,
-      module: {
-        rules: [...rules],
-      },
-      plugins: [
-        ...plugins,
-      ],
-      optimization: {
-        splitChunks: {
-          cacheGroups: {
-            vendor: {
-              name: 'vendor',
-              chunks: 'all',
-              test: /node_modules/,
-            },
+module.exports = [
+  {
+    entry: {
+      client: path.resolve(__dirname, 'src/client.js'),
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+      chunkFilename: '[name].js',
+      publicPath: '/',
+    },
+    devServer: {
+      port: 3000,
+      contentBase: './dist',
+      historyApiFallback: true,
+    },
+    resolve: resolve,
+    module: {
+      rules: [...rules],
+    },
+    plugins: [
+      ...plugins,
+      new LoadablePlugin({ writeToDisk: true }),
+    ],
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            name: 'vendor',
+            chunks: 'all',
+            test: /node_modules/,
           },
         },
       },
     },
-    {
-      target: 'node',
-      entry: {
-        prerender: path.resolve(__dirname, 'src/prerender.js'),
-      },
-      output: {
-        path: path.resolve(__dirname, 'dist'),
-        libraryTarget: 'commonjs2',
-        publicPath: '/',
-      },
-      resolve: resolve,
-      module: {
-        rules: [...rules],
-      },
-      plugins: [
-        ...plugins,
-        new StaticSiteGeneratorPlugin({
-          entry: 'prerender',
-          paths: [
-            '/',
-            '/kidpix',
-            '/kidpix/zine',
-            '/kidpix/tutorial',
-          ],
-          locals: {},
-        }),
-      ],
+  },
+  {
+    target: 'node',
+    entry: {
+      prerender: path.resolve(__dirname, 'src/prerender.js'),
     },
-  ];
-};
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: 'commonjs2',
+      publicPath: '/',
+    },
+    resolve: resolve,
+    module: {
+      rules: [...rules],
+    },
+    plugins: [
+      ...plugins,
+      new WaitPlugin('dist/loadable-stats.json'),
+      new StaticSiteGeneratorPlugin({
+        entry: 'prerender',
+        paths: [
+          '/',
+          '/kidpix',
+          '/kidpix/zine',
+          '/kidpix/tutorial',
+        ],
+        locals: {},
+      }),
+    ],
+  },
+];
